b7c3c633776316d54df969738c480fc948815437
masamasa9841 <masamasa9841@gmail.com>
2017-10-30 18:14:22 +0900

    viewportの再設定。とりあえず動く

--- resources/templates/layout/header/header.php
+++ resources/templates/layout/header/header.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
   <title><?php wp_title('|', true, 'right'); ?></title>
   <?php wp_enqueue_script('jquery');//jQueryの読み込み?>
   <?php wp_head(); ?>
