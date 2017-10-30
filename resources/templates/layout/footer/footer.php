91ca0d94299d74aa0fc70dc1b956ea4892a24827
masamasa9841 <masamasa9841@gmail.com>
2017-10-30 17:44:43 +0900

    footerをいい感じに

--- resources/templates/layout/footer/footer.php
+++ resources/templates/layout/footer/footer.php
@@ -3,9 +3,9 @@
       </section>
     </main>
     <?php get_sidebar(); ?>
-  </div><!-- container -->
-  <footer id="hoge">
-    <small>&copy; <?php bloginfo('name'); ?> All Rights Reserved.</small>
+  <footer id="global-footer">
+    &copy; <?php bloginfo('name'); ?> All Rights Reserved.
   </footer>
+  </div><!-- container -->
   <?php get_template_part('template-parts/nav'); //フッターモバイルボタンのテンプレート?>
 </html>
