030815f956c1977bf6b6ca2642d7a9c85c3a9b14
masamasa9841 <masamasa9841@gmail.com>
2017-11-02 17:04:39 +0900

    キモかったので再度調整

--- resources/src/js/get-width.js
+++ resources/src/js/get-width.js
@@ -4,7 +4,7 @@
     var window_max_Width = $(window).width();
     var navigation_Width = $(".scroll").outerWidth();
     var sidebar_Width = $(".sidebar").outerWidth();
-    var max_main_widh = window_max_Width - navigation_Width - sidebar_Width;
+    var max_main_widh = window_max_Width - navigation_Width - sidebar_Width -20; // 20は右の隙間
     $('.main-contents').css('width',max_main_widh); // css追加
     // console.log(max_main_widh);
   });
