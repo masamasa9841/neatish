5725503b8bb902ba3ca7cf30abd0b8eb4b06e8c5
masamasa9841 <masamasa9841@gmail.com>
2017-11-02 19:42:32 +0900

    パンくずリストの追加(プラグイン)

---
+++ resources/template-parts/articleinfo.php
@@ -0,0 +1,17 @@
+<?php // Article Info ?>
+
+<div class="article-info">
+	<h4>Article Info</h4>
+		<p>Created:  <?php the_date('Y/m/d \a\t g:i A'); ?></p>
+		<p>Modified: <?php the_modified_date('Y/m/d \a\t g:i A'); ?></p>
+		<p><?php if (function_exists ('wpp_get_views')) {
+				echo 'Views: ' . wpp_get_views ( get_the_ID() ); } ?></p>
+		<p class="category">Category: <?php the_category(', ') ?></p>
+		<div>
+			<?php if(get_previous_post()): // 前の記事があったら?>
+			  <span class="prev">Prev: <?php previous_post_link('%link','%title'); ?></span>
+			<?php endif; if(get_next_post()): // 次の記事があったら?>
+			  <sapn class="next">Next: <?php next_post_link('%link','%title' ); ?></span>
+			<?php endif; ?>
+		</div>
+</div>
--- resources/templates/layout/single/single.php
+++ resources/templates/layout/single/single.php
@@ -1,36 +1,21 @@
-<div role="main">
-    <?php if (have_posts()) :
-    while (have_posts()) : the_post(); ?>
-      <article class="article">
-        <header>
-         <h1><?php the_title_attribute(); ?></h1>
-        </header>
+<div class="entry-body" role="main">
+	<?php if (have_posts()) :
+		while (have_posts()) : the_post(); ?>
+		  <?php get_template_part('template-parts/breadcrumbs'); // パンくずリスト?>
+		  <article class="article">
+			  <header>
+					<h1><?php the_title_attribute(); ?></h1>
+				</header>
+			  <?php if(has_post_thumbnail()) {the_post_thumbnail(); } ?>
+			  <?php the_content(); ?>
+		  </article>
+	  <?php endwhile; ?>
 
-        <?php if(has_post_thumbnail()) {the_post_thumbnail(); } ?>
-        <?php the_content(); ?>
-      </article>
+	<?php get_template_part('template-parts/articleinfo');// ArticleInfoのテンプレート?>
 
-    <?php endwhile; ?>
+		<?php comments_template(); ?>
 
-		<div class="article-info">
-			<h4>Article Info</h4>
-						<p>Created: <?php the_date('c'); ?></p>
-						<p>Modified: <?php the_modified_date('c'); ?></p>
-						<p><?php if (function_exists ('wpp_get_views')) {
-							 echo 'Views: ' . wpp_get_views ( get_the_ID() ); } ?></p>
-						<p class="category">Category: <?php the_category(', ') ?></p>
-						<div>
-						<?php if(get_previous_post()): // 前の記事があったら?>
-						<span class="prev">Prev: <?php previous_post_link('%link','%title'); ?></span>
-						<?php endif; if(get_next_post()): // 次の記事があったら?>
-						<sapn class="next">Next: <?php next_post_link('%link','%title' ); ?></span>
-						<?php endif; ?>
-						</div>
-		</div>
-
-      <?php comments_template(); ?>
-
-   <?php else : ?>
-      <p>お探しの記事は見つかりませんでした。</p>
-    <?php endif; ?>
-    </div><!-- /#main -->
+	<?php else : ?>
+		<p>お探しの記事は見つかりませんでした。</p>
+	<?php endif; ?>
+</div><!-- /#main -->
