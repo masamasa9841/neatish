82093b6a195454b7a0e54d101c66522d92d17886
masamasa9841 <masamasa9841@gmail.com>
2017-11-03 16:33:44 +0900

    sharebuttonの一時停止

--- resources/template-parts/share-buttons.php
+++ resources/template-parts/share-buttons.php
@@ -1,16 +1,21 @@
 <ul class="web-logo-font">
   <li class="twitter-btn-icon">
-    <a href="#" class="twitter-btn-icon-link"><span class="icon-twitter"></span></a>
+    <a href="//twitter.com/share" class="twitter-btn-icon-link"><span class="icon-twitter"></span></a>
   </li>
   <li class="facebook-btn-icon">
     <a href="#" class="facebook-btn-icon-link"><span class="icon-facebook"></span></a>
   </li>
   <li class="google-plus-btn-icon">
     <a href="#" class="google-plus-btn-icon-link"><span class="icon-googleplus"></span></a>
-  </li>
-  <li class="hatena-btn-icon">
-    <a href="#" class="hatena-btn-icon-link"><span class="icon-hatena"></span></a>
-  </li>
+	</li>
+
+	<li class="hatena-btn">
+		<a href="//b.hatena.ne.jp/entry/<?php the_permalink(); ?>" class="hatena-btn-icon-link" data-hatena-bookmark-title="<?php the_title(); ?>｜<?php bloginfo('name'); ?>">
+			<span class="icon-hatena"></span>
+		</a>
+		<script type="text/javascript" src="//b.st-hatena.com/js/bookmark_button.js" async="async"></script>
+	</li>
+
   <li class="pocket-btn-icon">
     <a href="#" class="pocket-btn-icon-link"><span class="icon-pocket"></a>
   </li>
@@ -21,4 +26,4 @@
     <a href="#" class="feedly-btn-icon-link"><span class="icon-feedly"></a>
   </li>
 </ul>
-<div style="clear:both;"></div>
+<div style="clear:both;"></div> <!-- 回り込みの解除 -->
--- resources/templates/layout/single/single.php
+++ resources/templates/layout/single/single.php
@@ -10,7 +10,6 @@
 			  <?php the_content(); ?>
 		  </article>
 	  <?php endwhile; ?>
-  <?php get_template_part('template-parts/share-buttons'); // パンくずリスト?>
 	<?php get_template_part('template-parts/articleinfo');// ArticleInfoのテンプレート?>
 
 		<?php comments_template(); ?>
