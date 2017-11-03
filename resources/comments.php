1bf68ab9db8c81d34ff330176bd8a17780d03892
masamasa9841 <masamasa9841@gmail.com>
2017-11-03 16:35:46 +0900

    コメントの停止

--- resources/comments.php
+++
@@ -1,8 +0,0 @@
-<div id="comments" class="comment_form">
-  <?php if(have_comments()): ?>
-    <h1 class="comments-title">Comment</h1>
-    <ul class="comments-list">
-      <?php wp_list_comments('avatar_size=60'); ?>
-    </ul>
-  <?php endif; ?>
-</div>
