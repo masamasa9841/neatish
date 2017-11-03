73b2c908426b3eb7b644270f07fd8a39ad4c755b
masamasa9841 <masamasa9841@gmail.com>
2017-11-03 13:30:23 +0900

    テストshareボタン

---
+++ resources/template-parts/share-buttons.php
@@ -0,0 +1,24 @@
+<ul class="web-logo-font">
+  <li class="twitter-btn-icon">
+    <a href="#" class="twitter-btn-icon-link"><span class="icon-twitter"></span></a>
+  </li>
+  <li class="facebook-btn-icon">
+    <a href="#" class="facebook-btn-icon-link"><span class="icon-facebook"></span></a>
+  </li>
+  <li class="google-plus-btn-icon">
+    <a href="#" class="google-plus-btn-icon-link"><span class="icon-googleplus"></span></a>
+  </li>
+  <li class="hatena-btn-icon">
+    <a href="#" class="hatena-btn-icon-link"><span class="icon-hatena"></span></a>
+  </li>
+  <li class="pocket-btn-icon">
+    <a href="#" class="pocket-btn-icon-link"><span class="icon-pocket"></a>
+  </li>
+  <li class="line-btn-icon">
+    <a href="#" class="line-btn-icon-link"><span class="icon-line"></a>
+  </li>
+  <li class="feedly-btn-icon">
+    <a href="#" class="feedly-btn-icon-link"><span class="icon-feedly"></a>
+  </li>
+</ul>
+<div style="clear:both;"></div>
