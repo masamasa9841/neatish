442b86f6f9ac646465795c5ba2040b6c3a7717e0
Masaya Okawa <masamasa9841@gmail.com>
2017-11-04 14:42:50 +0900

    インデントの修正

--- resources/template-parts/articleinfo.php
+++ resources/template-parts/articleinfo.php
@@ -9,9 +9,9 @@
 		<p class="category">Category: <?php the_category(', ') ?></p>
 		<div>
 			<?php if(get_previous_post()): // 前の記事があったら?>
-			  <span class="prev">Prev: <?php previous_post_link('%link','%title'); ?></span>
+				<span class="prev">Prev: <?php previous_post_link('%link','%title'); ?></span>
 			<?php endif; if(get_next_post()): // 次の記事があったら?>
-			  <sapn class="next">Next: <?php next_post_link('%link','%title' ); ?></span>
+				<sapn class="next">Next: <?php next_post_link('%link','%title' ); ?></span>
 			<?php endif; ?>
 		</div>
 </div>
