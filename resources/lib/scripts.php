4587249426b9ac0f6b40a8b597506233a8f2a93e
Masaya Okawa <masamasa9841@gmail.com>
2017-11-04 17:24:06 +0900

    navigationをjsで読み込む事にした(スッキリ)。

--- resources/lib/scripts.php
+++ resources/lib/scripts.php
@@ -1,24 +1,45 @@
-<?php //cssの読み込み。headの中には書かない
-if ( !function_exists( 'add_filis' ) ):
-    function add_filis() {
+<?php //css, jsの読み込み。headの中には書かない
+	if ( !function_exists( 'add_filis' ) ):
+		function add_filis() {
+			// テーマスタイルの呼び出し
+			wp_enqueue_style( 'main-style',
+				get_template_directory_uri() . '/src/css/style.css'
+			);
 
-        // テーマスタイルの呼び出し
-        wp_enqueue_style( 'main-style',
-            get_template_directory_uri() . '/src/css/style.css'
-				);
+			// 画面の配置(width)を指定するスクリプト
+			wp_enqueue_script(
+				'get-width',
+				get_stylesheet_directory_uri() . '/src/js/get-width.js',
+				array( 'jquery' )
+			);
 
-				wp_enqueue_script(
-					'get-width',
-					get_stylesheet_directory_uri() . '/src/js/get-width.js',
-					array( 'jquery' )
-				);
+			// シェアボタンのカウントをするスクリプト
+			wp_enqueue_script(
+				'share-count',
+				get_stylesheet_directory_uri() . '/src/js/share-count.js',
+				array( 'jquery' )
+			);
 
-				wp_enqueue_script(
-					'share-count',
-					get_stylesheet_directory_uri() . '/src/js/share-count.js',
-					array( 'jquery' )
-				);
+			// ナヴィゲーションのスクロール
+			wp_enqueue_script(
+				'perfect-scrollbar',
+				get_stylesheet_directory_uri() . '/src/js/perfect-scrollbar.min.js',
+				array( 'jquery' )
+			);
 
-    }
-endif;
+			// ナビゲーションのサブメニューの動き
+			wp_enqueue_script(
+				'velocity',
+				get_stylesheet_directory_uri() . '/src/js/velocity.min.js',
+				array( 'jquery' )
+			);
+
+			// ナビゲーションの全体のjs
+			wp_enqueue_script(
+				'navigation',
+				get_stylesheet_directory_uri() . '/src/js/navigation.js',
+				array( 'jquery', 'velocity', 'perfect-scrollbar' )
+			);
+		}
+	endif;
 add_action( 'wp_enqueue_scripts', 'add_filis', 1 );
--- resources/templates/layout/footer/footer.php
+++ resources/templates/layout/footer/footer.php
@@ -1,12 +1,11 @@
-          <?php wp_footer(); ?>
-        </body>
-      </section>
-    </main>
-    <?php get_sidebar(); ?>
-      <footer class="global-footer">
-        &copy; <?php bloginfo('name'); ?> All Rights Reserved.
-      </footer>
-    </div><!-- container -->
-    <?php get_template_part('template-parts/nav'); //フッターモバイルボタンのテンプレート?>
+					<?php wp_footer(); ?>
+				</body>
+			</section>
+		</main>
+		<?php get_sidebar(); // サイドバーの呼び出し?>
+			<footer class="global-footer">
+				&copy; <?php bloginfo('name'); ?> All Rights Reserved.
+			</footer>
+		</div><!-- container -->
+		<!-- <?php get_template_part('template-parts/nav'); //フッターモバイルボタンのテンプレート?> -->
 </html>
-
