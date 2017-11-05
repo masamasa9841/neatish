ebde7156b56b9c62027f45aa01c7b2d769fd649f
Masaya Okawa <masamasa9841@gmail.com>
2017-11-06 01:12:59 +0900

    英語に直した

--- resources/functions.php
+++ resources/functions.php
@@ -7,47 +7,54 @@
  * @license GPL-2.0+
  */
 
-// script集.
+// Script collection.
 require get_template_directory() . '/lib/scripts.php';
 
-// snsのscript.
+// SNS Script.
 require get_template_directory() . '/lib/sns.php';
 
 // widetエリアのカスタマイズ.
 require get_template_directory() . '/template-parts/widet-categorize.php';
 
 
-// サムネイルのサイズ.
+// Size of Thumbnail.
 add_image_size( 'thumb320', 320, 180, true );
 add_image_size( 'thumb150', 150, 150, true );
 add_image_size( 'thumbnail_200_140', 200, 140, true );
 
-// アイキャッチの有効.
+// Eye catching enabled.
 add_theme_support( 'post-thumbnails' );
 
-register_sidebar(array(
-	'name'          => 'サイドバーウェジェット',
-	'id'            => 'sidebar',
-	'description'   => '',
-	'before_widget' => '<section id="widget" class="%2$s">',
-	'after_widget'  => '</section>',
-	'before_title'  => '<h2>',
-	'after_title'   => '</h2>',
-));
+/**
+ *
+ * Widget enabled.
+ */
+function neatish_widgets_init() {
+	register_sidebar(array(
+		'name'          => 'サイドバーウェジェット',
+		'id'            => 'sidebar',
+		'description'   => '',
+		'before_widget' => '<section id="widget" class="%2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2>',
+		'after_title'   => '</h2>',
+	));
+}
+add_action( 'widgets_init', 'neatish_widgets_init' );
 
 /**
- * 本文抜粋を取得する関数
+ * Script to get a snippet excerpt.
  *
  * @param string $content Content.
  * @param int    $length  Hope Content length.
  */
 function get_the_custom_excerpt( $content, $length ) {
 
-	$length  = ( $length ? $length : 70 ); // デフォルトの長さを指定する.
-	$content = preg_replace( '/<!--more-->.+/is', '', $content ); // moreタグ以降削除.
-	$content = strip_shortcodes( $content );// ショートコード削除.
-	$content = strip_tags( $content ); // タグの除去.
-	$content = str_replace( '&nbsp;', '', $content ); // 特殊文字の削除（今回はスペースのみ）.
-	$content = mb_substr( $content, 0, $length );// 文字列を指定した長さで切り取る.
+	$length  = ( $length ? $length : 70 );
+	$content = preg_replace( '/<!--more-->.+/is', '', $content );
+	$content = strip_shortcodes( $content );
+	$content = strip_tags( $content );
+	$content = str_replace( '&nbsp;', '', $content );
+	$content = mb_substr( $content, 0, $length );
 	return $content;
 }
