087140168f8801710c744cae58eee98eadc71f9e
Masaya Okawa <masamasa9841@gmail.com>
2017-11-05 14:07:39 +0900

    少しh2を低くした

--- resources/template-parts/articleinfo.php
+++ resources/template-parts/articleinfo.php
@@ -1,17 +1,28 @@
-<?php // Article Info ?>
+<?php
+/**
+ * The template Articleinfo.
+ *
+ * @package neatish
+ * @author Masaya Okawa
+ * @license GPL-2.0+
+ */
+
+?>
 
 <div class="article-info">
 	<h4>Article Info</h4>
-		<p>Created:  <?php the_date('Y/m/d \a\t g:i A'); ?></p>
-		<p>Modified: <?php the_modified_date('Y/m/d \a\t g:i A'); ?></p>
-		<p><?php if (function_exists ('wpp_get_views')) {
-				echo 'Views: ' . wpp_get_views ( get_the_ID() ); } ?></p>
-		<p class="category">Category: <?php the_category(', ') ?></p>
+		<p>Created:  <?php the_date( 'Y/m/d \a\t g:i A' ); ?></p>
+		<p>Modified: <?php the_modified_date( 'Y/m/d \a\t g:i A' ); ?></p>
+		<p>
+			<?php if ( function_exists( 'wpp_get_views' ) ) { ?>
+				<?php echo 'Views: ' . esc_html( wpp_get_views( get_the_ID() ) ); } ?>
+		</p>
+		<p class="category">Category: <?php esc_html( the_category( ', ' ) ); ?></p>
 		<div>
-			<?php if(get_previous_post()): // 前の記事があったら?>
-				<span class="prev">Prev: <?php previous_post_link('%link','%title'); ?></span>
-			<?php endif; if(get_next_post()): // 次の記事があったら?>
-				<sapn class="next">Next: <?php next_post_link('%link','%title' ); ?></span>
+			<?php if ( get_previous_post() ) : ?>
+				<span class="prev">Prev: <?php previous_post_link( '%link', '%title' ); ?></span>
+			<?php endif; if ( get_next_post() ) : // 次の記事があったら. ?>
+				<sapn class="next">Next: <?php next_post_link( '%link', '%title' ); ?></span>
 			<?php endif; ?>
 		</div>
 </div>
