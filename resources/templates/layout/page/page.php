33810448eda85d854774c17a2c8f12d58a3460e1
Masaya Okawa <masamasa9841@gmail.com>
2017-11-05 21:31:57 +0900

    固定ページの追加

---
+++ resources/templates/layout/page/page.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying pages
+ *
+ * @package neatish
+ * @author Masaya Okawa
+ * @license GPL-2.0+
+ */
+
+?>
+<div class="entry-body" role="main">
+	<?php if ( have_posts() ) : ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'template-parts/breadcrumbs' ); // パンくずリスト. ?>
+			<article class="article">
+				<header>
+					<h1><?php the_title_attribute(); ?></h1>
+				</header>
+				<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+				<?php the_content(); ?>
+			</article>
+		<?php endwhile; ?>
+		<?php get_template_part( 'template-parts/share-buttons' ); // シェアボタンのテンプレート. ?>
+		<?php comments_template(); ?>
+	<?php else : ?>
+		<p>お探しの記事は見つかりませんでした。</p>
+	<?php endif; ?>
+</div><!-- /#main -->
