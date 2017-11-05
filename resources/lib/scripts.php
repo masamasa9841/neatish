3d5f58641f8368f31a4d4357f7b1cfe591433dd0
Masaya Okawa <masamasa9841@gmail.com>
2017-11-05 17:12:08 +0900

    Add comment

--- resources/lib/scripts.php
+++ resources/lib/scripts.php
@@ -1,45 +1,59 @@
-<?php //css, jsの読み込み。headの中には書かない
-	if ( !function_exists( 'add_filis' ) ):
-		function add_filis() {
-			// テーマスタイルの呼び出し
-			wp_enqueue_style( 'main-style',
-				get_template_directory_uri() . '/src/css/style.css'
-			);
+<?php
+/**
+ * CSS, JSの読み込み。headの中では行わない
+ *
+ * @package neatish
+ * @author Masaya Okawa
+ * @license GPL-2.0+
+ */
 
-			// 画面の配置(width)を指定するスクリプト
-			wp_enqueue_script(
-				'get-width',
-				get_stylesheet_directory_uri() . '/src/js/get-width.js',
-				array( 'jquery' )
-			);
+?>
 
-			// シェアボタンのカウントをするスクリプト
-			wp_enqueue_script(
-				'share-count',
-				get_stylesheet_directory_uri() . '/src/js/share-count.js',
-				array( 'jquery' )
-			);
+<?php
+if ( ! function_exists( 'add_filis' ) ) :
+	/**
+	 * ファイルを呼び出す
+	 */
+	function add_filis() {
+		// テーマスタイルの呼び出し.
+		wp_enqueue_style( 'main-style',
+			get_template_directory_uri() . '/src/css/style.css'
+		);
 
-			// ナヴィゲーションのスクロール
-			wp_enqueue_script(
-				'perfect-scrollbar',
-				get_stylesheet_directory_uri() . '/src/js/perfect-scrollbar.min.js',
-				array( 'jquery' )
-			);
+		// 画面の配置(width)を指定するスクリプト.
+		wp_enqueue_script(
+			'get-width',
+			get_stylesheet_directory_uri() . '/src/js/get-width.js',
+			array( 'jquery' )
+		);
 
-			// ナビゲーションのサブメニューの動き
-			wp_enqueue_script(
-				'velocity',
-				get_stylesheet_directory_uri() . '/src/js/velocity.min.js',
-				array( 'jquery' )
-			);
+		// シェアボタンのカウントをするスクリプト.
+		wp_enqueue_script(
+			'share-count',
+			get_stylesheet_directory_uri() . '/src/js/share-count.js',
+			array( 'jquery' )
+		);
 
-			// ナビゲーションの全体のjs
-			wp_enqueue_script(
-				'navigation',
-				get_stylesheet_directory_uri() . '/src/js/navigation.js',
-				array( 'jquery', 'velocity', 'perfect-scrollbar' )
-			);
-		}
+		// ナヴィゲーションのスクロール.
+		wp_enqueue_script(
+			'perfect-scrollbar',
+			get_stylesheet_directory_uri() . '/src/js/perfect-scrollbar.min.js',
+			array( 'jquery' )
+		);
+
+		// ナビゲーションのサブメニューの動き.
+		wp_enqueue_script(
+			'velocity',
+			get_stylesheet_directory_uri() . '/src/js/velocity.min.js',
+			array( 'jquery' )
+		);
+
+		// ナビゲーションの全体のjs.
+		wp_enqueue_script(
+			'navigation',
+			get_stylesheet_directory_uri() . '/src/js/navigation.js',
+			array( 'jquery', 'velocity', 'perfect-scrollbar' )
+		);
+	}
 	endif;
 add_action( 'wp_enqueue_scripts', 'add_filis', 1 );
