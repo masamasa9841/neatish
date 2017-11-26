f56daa5afbcbbbe4d199ba25e58f48ac14606de6
Masaya Okawa <masamasa9841@gmail.com>
2017-11-26 12:27:13 +0900

    mv file template

--- resources/index.php
+++ resources/index.php
@@ -7,6 +7,6 @@
  * @license GPL-2.0+
  */
 
-get_template_part( 'templates/layout/header/header' );
+get_header();
 get_template_part( 'templates/layout/list/list' );
-get_template_part( 'templates/layout/footer/footer' );
+get_footer();
