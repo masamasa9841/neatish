ed6b7dca493fec3efd5a9ec471d1f442f7bd2dd5
Masaya Okawa <masamasa9841@gmail.com>
2017-11-26 12:50:49 +0900

    mv body

--- resources/footer.php
+++ resources/footer.php
@@ -10,12 +10,12 @@
 ?>
 
 					<?php wp_footer(); ?>
-				</body>
-			</section>
-		</main>
-		<?php get_sidebar(); ?>
-		<footer class="global-footer">
-			&copy; <?php bloginfo( 'name' ); ?> All Rights Reserved.
-		</footer>
-	</div><!-- container -->
+				</section>
+			</main>
+			<?php get_sidebar(); ?>
+			<footer class="global-footer">
+				&copy; <?php bloginfo( 'name' ); ?> All Rights Reserved.
+			</footer>
+		</div><!-- container -->
+	</body>
 </html>
