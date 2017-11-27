bae5a43f7c17a6eda9d1a11326c9460345e6d08c
masamasa9841 <masamasa9841@gmail.com>
2017-11-27 20:15:18 +0900

    Fix file directory

--- resources/single.php
+++ resources/single.php
@@ -8,5 +8,35 @@
  */
 
 get_header();
-get_template_part( 'templates/layout/single/single' );
+?>
+
+<div class="entry-body" role="main">
+<?php if ( have_posts() ) : ?>
+	<?php
+	while ( have_posts() ) :
+		the_post();
+	?>
+		<?php get_template_part( 'template-parts/breadcrumbs' ); ?>
+		<?php wp_link_pages(); ?>
+		<article class="article">
+			<header>
+				<h1><?php the_title_attribute(); ?></h1>
+			</header>
+			<?php
+			if ( has_post_thumbnail() ) {
+				the_post_thumbnail();
+			}
+			?>
+			<?php the_content(); ?>
+		</article>
+	<?php endwhile; ?>
+	<?php get_template_part( 'template-parts/share-buttons' ); ?>
+	<?php get_template_part( 'template-parts/articleinfo' ); ?>
+	<?php comments_template(); ?>
+<?php else : ?>
+	<p>The article you were looking for could not be found.</p>
+<?php endif; ?>
+</div><!-- /#main -->
+<?php
+
 get_footer();
