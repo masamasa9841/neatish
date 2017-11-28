f809c60863952cbf6620d9ad89abde316921c434
masamasa9841 <masamasa9841@gmail.com>
2017-11-29 02:59:04 +0900

    カスタマイザー　test

--- resources/lib/css-csutom.php
+++ resources/lib/css-csutom.php
@@ -10,14 +10,31 @@
 $link_color       = get_theme_mod( 'link_color' );
 $background_color = get_theme_mod( 'background_color' );
 $theme_color      = get_theme_mod( 'theme_color' );
+$header_textcolor = get_theme_mod( 'header_textcolor' );
+$text_color = get_theme_mod( 'text_color' );
+$header_footer_color = get_theme_mod( 'header_footer_color' );
 ?>
 
 <?php echo "<style type='text/css'>"; ?>
 
+	/* header footer color */
+	.global-head,
+	.global-footer
+	{
+		background-color: <?php echo esc_html( $header_footer_color ); ?>
+	}
+
+	/* header text color */
+	.global-head a{
+		color: #<?php echo esc_html( $header_textcolor ); ?>
+	}
+
+	/* link-color */
 	a {
 		color: <?php echo esc_html( $link_color ); ?>
 	}
 
+	/* background color */
 	.container {
 		background-color: #<?php echo esc_html( $background_color ); ?>
 	}
@@ -25,13 +42,17 @@ $theme_color      = get_theme_mod( 'theme_color' );
 	.scroll,
 	.nav-menu-head,
 	.widget_categories li,
+	h3,
 	.tagcloud a{
 		background: <?php echo esc_html( $theme_color ); ?>
 	}
-	.global-head a,
 	.global-footer,
-	.web-logo-font li a,
-	h2::first-letter{
+	blockquote::before {
 		color: <?php echo esc_html( $theme_color ); ?>
 	}
+
+	body {
+		color: <?php echo esc_html( $text_color ); ?>
+	}
+
 <?php echo '</style>'; ?><j:w></j:w>
--- resources/lib/customizer.php
+++ resources/lib/customizer.php
@@ -7,10 +7,6 @@
  * @license GPL-2.0+
  */
 
-define( 'LINK_COLOR', '' );
-define( 'BACKGROUND_COLOR_NEATISH', 'ffffff' );
-define( 'THEME_COLOR', '' );
-define( 'THEME_SUB_COLOR', '' );
 
 /**
  * Script to get a snippet excerpt.
@@ -25,41 +21,64 @@ function theme_customize_register( $wp_customize ) {
 		'priority'    => 1,
 	) );
 
+	// header, footer color.
+	$wp_customize->add_setting( 'header_footer_color', array(
+		'sanitize_callback' => 'sanitize_hex_color',
+	) );
+	$wp_customize->add_control( new WP_Customize_Color_Control(
+		$wp_customize,
+		'header_footer_color', array(
+			'label'       => 'Header Footer color',
+			'section'     => 'colors',
+			'settings'    => 'header_footer_color',
+			'priority'    => 7,
+		)
+	) );
+
 	// background_color for reflesh.
 	$wp_customize->add_setting( 'background_color', array(
 		'default' => 'ffffff',
 	) );
-	// Remove header color.
-	$wp_customize->remove_control( 'header_textcolor' );
 
 	// link color.
 	$wp_customize->add_setting( 'link_color', array(
-		'default'           => LINK_COLOR,
 		'sanitize_callback' => 'sanitize_hex_color',
 	) );
 	$wp_customize->add_control( new WP_Customize_Color_Control(
 		$wp_customize,
 		'link_color', array(
-			'label'       => 'link color',
-			'description' => 'Change link color',
+			'label'       => 'Link color',
 			'section'     => 'colors',
 			'settings'    => 'link_color',
 			'priority'    => 15,
 		)
 	) );
+
 	// Theme Color.
 	$wp_customize->add_setting( 'theme_color', array(
-		'default'           => THEME_COLOR,
 		'sanitize_callback' => 'sanitize_hex_color',
 	) );
 	$wp_customize->add_control( new WP_Customize_Color_Control(
 		$wp_customize,
 		'theme_color', array(
-			'label'       => 'theme color',
-			'description' => 'Change theme color',
+			'label'       => 'Theme color',
 			'section'     => 'colors',
 			'settings'    => 'theme_color',
 			'priority'    => 20,
+		)
+	) );
+
+	// Text Color.
+	$wp_customize->add_setting( 'text_color', array(
+		'sanitize_callback' => 'sanitize_hex_color',
+	) );
+	$wp_customize->add_control( new WP_Customize_Color_Control(
+		$wp_customize,
+		'text_color', array(
+			'label'       => 'Text color',
+			'section'     => 'colors',
+			'settings'    => 'text_color',
+			'priority'    => 25,
 		)
 	) );
 }
